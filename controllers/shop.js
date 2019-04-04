76a2dfc20f37d985568d12c2e25ea7d849813eb3
sadiq79 <sadiq.md.ali@gmail.com>
2019-04-04 16:20:40 +0530

    integrate react-express

--- controllers/shop.js
+++ controllers/shop.js
@@ -18,19 +18,19 @@ exports.getProducts = (req, res, next) => {
     .then(numProducts => {
       totalItems = numProducts;
       return Product.find()
-        .skip((page - 1) * ITEMS_PER_PAGE)
-        .limit(ITEMS_PER_PAGE);
+        // .skip((page - 1) * ITEMS_PER_PAGE)
+        // .limit(ITEMS_PER_PAGE);
     })
     .then(products => {
-      res.status(200).json({
-        prods: products
+      res.status(200).json(
+        products
         // currentPage: page,
         // hasNextPage: ITEMS_PER_PAGE * page < totalItems,
         // hasPreviousPage: page > 1,
         // nextPage: page + 1,
         // previousPage: page - 1,
         // lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
-      });
+      );
     })
     .catch(err => {
       const error = new Error(err);
